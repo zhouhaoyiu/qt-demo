14f783f69707c31c0a4f95aaa6185f1e1f890c4a
zhouhaoyu <67226385+zhouhaoyiu@users.noreply.github.com>
2022-06-13 21:43:14 +0800

    change to get fullname
    Signed-off-by: zhouhaoyu <67226385+zhouhaoyiu@users.noreply.github.com>

---
+++ widget.cpp
@@ -0,0 +1,32 @@
+﻿#include "widget.h"
+#include "ui_widget.h"
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    ui->firstname->setText("");
+    ui->lastname->setText("");
+
+    connect(ui->getNameBtn,SIGNAL(clicked()),this,SLOT(getFullName()));
+    connect(ui->clear,SIGNAL(clicked()),this,SLOT(clear()));
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::getFullName()
+{
+    fullname = ui->firstname->text()+ui->lastname->text();
+    ui->fullname->setText(fullname);
+}
+
+void Widget::clear()
+{
+    ui->firstname->clear();
+    ui->lastname->clear();
+    ui->fullname->clear();
+}
---
+++ widget.h
@@ -0,0 +1,27 @@
+﻿#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+
+namespace Ui {
+class Widget;
+}
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Widget(QWidget *parent = 0);
+    ~Widget();
+
+private:
+    Ui::Widget *ui;
+    QString fullname;
+
+private slots:
+    void getFullName();
+    void clear();
+};
+
+#endif // WIDGET_H
